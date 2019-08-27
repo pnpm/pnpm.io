7d7126b5001cca8ae9025569de3af98a72ff189c
Connor Hanson <connorh999@yahoo.com>
2019-08-27 02:37:25 -0500

    fix: correct linking for 'getting started' page

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -8,18 +8,12 @@
 const React = require('react');
 
 class Footer extends React.Component {
-  docUrl(doc, language) {
-    const baseUrl = this.props.config.baseUrl;
-    return baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
-  }
-
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
     return baseUrl + (language ? language + '/' : '') + doc;
   }
 
   render() {
-    const currentYear = new Date().getFullYear();
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
@@ -35,7 +29,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('installation.html', this.props.language)}>
+            <a href={this.pageUrl('installation.html', this.props.language)}>
               Getting Started
             </a>
           </div>
