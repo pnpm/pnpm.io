3d50e47cbe945a126ecfa4e6d9beccb15a1f50b0
Juan Picado <juanpicado19@gmail.com>
2022-02-26 13:44:42 +0100

    fix build

--- src/pages/versions.js
+++ src/pages/versions.js
@@ -10,7 +10,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 
-import {useVersions, useLatestVersion} from '@theme/hooks/useDocs';
+import {useVersions, useLatestVersion} from '@docusaurus/plugin-content-docs/client';
 
 function Version() {
   const {siteConfig} = useDocusaurusContext();
