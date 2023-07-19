87bdfbb00d5bc05ce236accb7c01c65439698303
Zoltan Kochan <z@kochan.io>
2023-07-19 15:20:54 +0300

    Revert "update sponsors list"
    This reverts commit 75c252b8b56d411e9b77f0c8a1ddfe666858b2ce.

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,6 +90,9 @@ const Showcase = props => {
               flexFlow: 'row wrap',
               justifyContent: 'center',
             }}>
+              <a href={addUTM("https://bit.dev/")} target="_blank">
+                <img style={{padding: '20px'}} width="120" alt="bit" src="/img/users/bit.svg" />
+              </a>
               <a href={addUTM("https://novu.co/")} target="_blank">
                 <img style={{padding: '20px'}} width="240" alt="Novu" src="/img/users/novu.svg" />
               </a>
