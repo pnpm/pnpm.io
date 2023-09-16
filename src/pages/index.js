7a01826d845881221b796f7bb0c79cac98c950e3
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-09-16 11:31:54 +0100

    chore: update Depot logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -109,7 +109,7 @@ const Showcase = props => {
                 <img style={{padding: '20px'}} width="240" alt="Vercel" src="/img/users/vercel.svg" />
               </a>
               <a href={addUTM("https://depot.dev/")} target="_blank">
-                <img style={{padding: '20px'}} width="240" alt="Depot" src="/img/users/depot.svg" />
+                <img style={{padding: '20px'}} width="240" alt="Depot" src="/img/users/depot_dynamic.svg" />
               </a>
               <a href={addUTM("https://moonrepo.dev/")} target="_blank">
                 <img style={{padding: '20px'}} width="240" alt="Moonrepo" src="/img/users/moonrepo.svg" />
