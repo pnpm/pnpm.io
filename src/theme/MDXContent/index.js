08157aef26c5c0f0b284f87b3ca792ddb942d4a3
Zoltan Kochan <z@kochan.io>
2023-10-12 02:24:35 +0300

    Revert "avoid minification"
    This reverts commit 124cb915d6e44c790720569a7a641a69ac27b972.

--- src/theme/MDXContent/index.js
+++ src/theme/MDXContent/index.js
@@ -8,31 +8,30 @@ export default function MDXContentWrapper(props) {
       <MDXContent {...props} />
     <script src="//m.servedby-buysellads.com/monetization.custom.js"></script>
     <script dangerouslySetInnerHTML={{__html: `
-// @__PURE__
-(function() {
-  if (typeof _bsa !== 'undefined' && _bsa) {
-    _bsa.init('custom', 'CEAIPK3U', 'placement:pnpmio', {
-      target: '#bsa-custom-01',
-      template: \`
-<a href="##link##" class="native-banner" style="background: ##backgroundColor##" rel="sponsored noopener" target="_blank" title="##company## — ##tagline##">
-  <img class="native-img" width="125" src="##logo##" />
-  <div class="native-main">
-    <div class="native-details" style="
-        color: ##textColor##;
-        border-left: solid 1px ##textColor##;
-      ">
-      <span class="native-desc">##description##</span>
-    </div>
-    <span class="native-cta" style="
-        color: ##ctaTextColor##;
-        background-color: ##ctaBackgroundColor##;
-      ">##callToAction##</span>
-  </div>
-</a>
-\`,
-    });
-  }
-})();`}} >
+    (function() {
+      if (typeof _bsa !== 'undefined' && _bsa) {
+        _bsa.init('custom', 'CEAIPK3U', 'placement:pnpmio', {
+          target: '#bsa-custom-01',
+          template: \`
+    <a href="##link##" class="native-banner" style="background: ##backgroundColor##" rel="sponsored noopener" target="_blank" title="##company## — ##tagline##">
+      <img class="native-img" width="125" src="##logo##" />
+      <div class="native-main">
+        <div class="native-details" style="
+            color: ##textColor##;
+            border-left: solid 1px ##textColor##;
+          ">
+          <span class="native-desc">##description##</span>
+        </div>
+        <span class="native-cta" style="
+            color: ##ctaTextColor##;
+            background-color: ##ctaBackgroundColor##;
+          ">##callToAction##</span>
+      </div>
+    </a>
+    \`,
+        });
+      }
+    })();`}} >
     </script>
     </>
   );
