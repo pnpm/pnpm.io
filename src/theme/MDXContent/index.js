6f7879013a5c675f68f6cb614c741875f5306741
Zoltan Kochan <z@kochan.io>
2023-10-12 00:14:53 +0300

    docs: add carbon ads to content

---
+++ src/theme/MDXContent/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MDXContent from '@theme-original/MDXContent';
+
+export default function MDXContentWrapper(props) {
+  return (
+    <>
+      <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CEAIPK3U&placement=pnpmio" id="_carbonads_js"></script>
+      <MDXContent {...props} />
+    </>
+  );
+}
