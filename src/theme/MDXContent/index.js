ea4644a4a6f4f064c207384f9f6578a0cf92da8a
Zoltan Kochan <z@kochan.io>
2023-10-12 01:04:14 +0300

    update ad

--- src/theme/MDXContent/index.js
+++ src/theme/MDXContent/index.js
@@ -4,7 +4,33 @@ import MDXContent from '@theme-original/MDXContent';
 export default function MDXContentWrapper(props) {
   return (
     <>
-      <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CEAIPK3U&placement=pnpmio" id="_carbonads_js"></script>
+      <div id="bsa-custom-01" class="bsa-standard"></div>
+<script dangerouslySetInnerHTML={{__html: `
+(function() {
+  if (typeof _bsa !== 'undefined' && _bsa) {
+    _bsa.init('custom', 'CEAIPK3U', 'placement:pnpmio', {
+      target: '#bsa-custom-01',
+      template: \`
+<a href="##link##" class="native-banner" style="background: ##backgroundColor##" rel="sponsored noopener" target="_blank" title="##company## — ##tagline##">
+	<img class="native-img" width="125" src="##logo##" />
+	<div class="native-main">
+		<div class="native-details" style="
+				color: ##textColor##;
+				border-left: solid 1px ##textColor##;
+			">
+			<span class="native-desc">##description##</span>
+		</div>
+		<span class="native-cta" style="
+				color: ##ctaTextColor##;
+				background-color: ##ctaBackgroundColor##;
+			">##callToAction##</span>
+	</div>
+</a>
+\`,
+    });
+  }
+})();`}} >
+</script><script src="//m.servedby-buysellads.com/monetization.custom.js"></script>
       <MDXContent {...props} />
     </>
   );
