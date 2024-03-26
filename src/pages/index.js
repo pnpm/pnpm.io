5ef8b8145c80061ce7f3e0730fbb55890acd3423
Zoltan Kochan <z@kochan.io>
2024-03-26 20:36:16 +0100

    docs: add new sponsor

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,6 +133,9 @@ const Showcase = props => {
               <a href={addUTM("https://cerbos.dev/")} target="_blank">
                 <img style={{padding: '20px'}} width="200" alt="Cerbos" src="/img/users/cerbos.svg" />
               </a>
+              <a href={addUTM("https://vpsserver.com/en-us/")} target="_blank">
+                <img style={{padding: '20px'}} width="200" alt="Cerbos" src="/img/users/vpsserver.svg" />
+              </a>
             </div>
           </div>
           <div>
