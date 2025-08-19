a40b087b9c08c07799b6c346c595be16441bf1b4
Tatsunori Uchino <tats.u@live.jp>
2025-08-20 01:45:38 +0900

    feat: update React & Docusaurus & prism-react-renderer (#698)

--- benchmarks/fixtures/react-app/src/index.js
+++ benchmarks/fixtures/react-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 registerServiceWorker();
